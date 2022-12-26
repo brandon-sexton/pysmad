922e739def252d72ebb53d8ad792e500c7d64409
Brandon Sexton <brandon.sexton.1@outlook.com>
2022-12-26 16:10:15 -0700

    removed sphinx from setup

--- setup.py
+++ setup.py
@@ -3,26 +3,12 @@
 import pathlib
 
 from setuptools import find_packages, setup
-from sphinx.setup_command import BuildDoc
 
 here = pathlib.Path(__file__).parent.resolve()
-cmdclass = {"build_sphinx": BuildDoc}
-name = "pyxis"
-version = "0.1"
-release = "0.1.0"
+
 setup(
-    name=name,
+    name="pyxis",
     author="Brandon Sexton",
-    version=release,
-    cmdclass=cmdclass,
+    version="0.1.0",
     packages=find_packages(),
-    # these are optional and override conf.py settings
-    command_options={
-        "build_sphinx": {
-            "project": ("setup.py", name),
-            "version": ("setup.py", version),
-            "release": ("setup.py", release),
-            "source_dir": ("setup.py", "./docs"),
-        }
-    },
 )
