2588baf2cda4a936e6da443357c789bc8f24593d
Brandon Sexton <brandon.sexton.1@outlook.com>
2022-12-26 06:46:02 -0700

    added sphinx documentation

--- setup.py
+++ setup.py
@@ -3,7 +3,26 @@
 import pathlib
 
 from setuptools import find_packages, setup
+from sphinx.setup_command import BuildDoc
 
 here = pathlib.Path(__file__).parent.resolve()
-
-setup(name="pyxis", version="0.1.0", packages=find_packages())
+cmdclass = {"build_sphinx": BuildDoc}
+name = "pyxis"
+version = "0.1"
+release = "0.1.0"
+setup(
+    name=name,
+    author="Brandon Sexton",
+    version=release,
+    cmdclass=cmdclass,
+    packages=find_packages(),
+    # these are optional and override conf.py settings
+    command_options={
+        "build_sphinx": {
+            "project": ("setup.py", name),
+            "version": ("setup.py", version),
+            "release": ("setup.py", release),
+            "source_dir": ("setup.py", "./docs"),
+        }
+    },
+)
