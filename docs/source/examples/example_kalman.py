99215c85cadaadc6a0ec2ded07caa9ab7d1a540c
Brandon Sexton <brandon.sexton.1@outlook.com>
2022-12-27 06:45:28 -0700

    added comments to example

--- docs/source/examples/example_kalman.py
+++ docs/source/examples/example_kalman.py
@@ -2,46 +2,60 @@ import matplotlib.pyplot as plt
 
 from pyxis.astro.bodies.artificial import Spacecraft
 from pyxis.astro.coordinates import GCRFstate, HillState
-from pyxis.astro.propagators.relative import Hill
 from pyxis.math.linalg import Vector3D
 from pyxis.time import Epoch
 
+# Define the initial time reference
 start_epoch: Epoch = Epoch.from_gregorian(2022, 12, 20, 0, 0, 0)
+
+# Create a desired relative state for the chase vehicle
 rel_chase_state = HillState(Vector3D(-11, 0, 0), Vector3D(0, 0, 0))
+
+# Create an inertial state for the target vehicle
 target_state: GCRFstate = GCRFstate(start_epoch, Vector3D(42164, 0, 0), Vector3D(0, 3.075, 0))
+
+# Create a spacecraft to act as an estimated state of the target spacecraft which will be used to initialize the filter
 seed: Spacecraft = Spacecraft(GCRFstate(start_epoch, Vector3D(42164.5, 0.5, 0.5), Vector3D(0, 3.075, 0)))
+
+# Create the chase vehicle's inertial state using the target state as the origin
 chase_state: GCRFstate = GCRFstate.from_hill(target_state, rel_chase_state)
+
+# Define a propagation epoch of one day
 end_epoch = start_epoch.plus_days(1)
 
+# Create the target and chase spacecraft
 chase: Spacecraft = Spacecraft(chase_state)
 target: Spacecraft = Spacecraft(target_state)
 
-rel = Hill(rel_chase_state, 42164)
+# Create empty lists to store data
+times, err = [], []
 
-hill_r = []
-hill_i = []
-rk_r = []
-rk_i = []
-times = []
-err = []
-
+# Initialize the kalman filter with the estimated state
 chase.acquire(seed)
+
+# Propagate
 while chase.current_epoch().value < end_epoch.value:
 
+    # Increment the epochs of the spacecraft
     chase.step()
     target.step_to_epoch(chase.current_epoch())
-    chase.track_state(target)
-    chase.process_wfov(target)
-    rel.step_by_seconds(chase.propagator.step_size)
 
+    # Update chase attitude to point payloads at target
+    chase.track_state(target)
+
+    # Create a simulated ob and feed it to the filter
+    chase.process_wfov(target)
+
+    # Get the relative truth state
     ric = chase.hill_position(target)
+
+    # Get the relative estimate from the chase solution
     nav = chase.filter.propagator.state.position
-    rk_r.append(ric.x)
-    rk_i.append(ric.y)
-    hill_r.append(rel.state.position.x)
-    hill_i.append(rel.state.position.y)
+
+    # Store data
     times.append(chase.current_epoch().value)
     err.append(abs(nav.magnitude() - ric.magnitude()))
 
+# Plot results
 plt.plot(times, err)
 plt.show()
