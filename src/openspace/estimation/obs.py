2e3ab8b2a6ecf6119df8d7ac8301a8b1e407851c
Brandon Sexton <brandon.sexton.1@outlook.com>
2023-01-15 07:39:49 -0700

    replaced position ob with space ob

--- src/openspace/estimation/obs.py
+++ src/openspace/estimation/obs.py
@@ -1,6 +1,5 @@
 from openspace.coordinates import GCRFstate, SphericalPosition
 from openspace.math.linalg import Vector3D
-from openspace.time import Epoch
 
 
 class SpaceObservation:
@@ -40,24 +39,3 @@ class SpaceObservation:
 
         #: one-sigma angular error of the observation in radians
         self.angular_error: float = ang_error
-
-
-class PositionOb:
-    def __init__(self, epoch: Epoch, position: Vector3D, error: float) -> None:
-        """class used to store an observation that contains position only
-
-        :param epoch: time of the observation
-        :type epoch: Epoch
-        :param position: observed position vector in km
-        :type position: Vector3D
-        :param error: error associated with the magnitude of the observed position vector in km
-        :type error: float
-        """
-        #: time of the observation
-        self.epoch: Epoch = epoch.copy()
-
-        #: observed position in km
-        self.position: Vector3D = position.copy()
-
-        #: error in km of the observed position magnitude
-        self.error: float = error
