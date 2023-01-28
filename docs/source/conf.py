79302349aa623187345afc510cfc87eeb3d92873
Brandon Sexton <brandon.sexton.1@outlook.com>
2023-01-28 09:05:40 -0700

    version increment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Brandon Sexton"
 author = "Brandon Sexton"
 
 # The full version, including alpha/beta/rc tags
-release = "2.5.0"
+release = "2.6.0"
 
 
 # -- General configuration ---------------------------------------------------
